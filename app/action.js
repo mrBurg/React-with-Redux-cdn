71a84d782383c379783cdb36449dc396a9b2b250
Fed <fedor.h@gmail.com>
2024-08-23 18:42:07 +0300

    Upload project with Redux Thunk

--- app/action.js
+++ app/action.js
@@ -3,5 +3,20 @@
 export const INCREMENT = 'counter/increment';
 export const DECREMENT = 'counter/decrement';
 
+/* export const fetchData = () => {
+  return (dispatch) => {
+    dispatch({ type: 'FETCH_DATA_REQUEST' });
+
+    fetch('https://api.example.com/data')
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: 'FETCH_DATA_SUCCESS', payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: 'FETCH_DATA_FAILURE', payload: error });
+      });
+  };
+}; */
+
 export const increment = (payload = 1) => ({ type: INCREMENT, payload });
 export const decrement = (payload = 1) => ({ type: DECREMENT, payload });
--- app/store.js
+++ app/store.js
@@ -2,9 +2,35 @@
 
 import { reducer, navbarReducer } from '/app/reducer.js';
 
-const { createStore, combineReducers } = Redux;
+const { createStore, combineReducers, applyMiddleware } = Redux;
+
+// console.log(ReduxThunk);
+
+function logger({ getState }) {
+  return (next) => (action) => {
+    console.log('will dispatch', action);
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action);
+
+    console.log('state after dispatch', getState());
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue;
+  };
+}
 
 export const store = createStore(
   combineReducers({ counter: reducer, navbar: navbarReducer }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  {},
+  applyMiddleware(
+    logger,
+    ReduxThunk
+    /* window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f */
+  )
 );
